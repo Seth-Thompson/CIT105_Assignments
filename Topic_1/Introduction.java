463c7967297d4f0a042261151878bc06864a3c19
solarizcool <solarizcool@gmail.com>
2018-10-28 21:10:13 -0500

    Week one introduction program with Java!

---
+++ Topic_1/Introduction.java
@@ -0,0 +1,18 @@
+// Seth Thompson
+// CST105
+// 10/21/2018
+/* This project is my own and all work on it is my own.*/
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		// Displays my name.
+		System.out.println("My name is Seth.");
+		// Displays my favorite color
+		System.out.println("My favorite color is green.");
+		// Displays what I would do if I won the lottery.
+		System.out.println("If I won the lottery, I would save it as the taxes are gonna be high on it and I might be short on cash one day.");
+
+	}
+
+}
